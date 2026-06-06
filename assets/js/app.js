bfbba434aa44793c91e075a0be599d563916aff4
digitallycoded <ccbadvancedocs@gmail.com>
2026-06-06 15:15:48 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,43 @@ async function login(){
 
   console.log(result);
 }
+
+async function login(){
+
+  const username =
+    document.getElementById(
+      "username"
+    ).value;
+
+  const password =
+    document.getElementById(
+      "password"
+    ).value;
+
+  google.script.run
+    .withSuccessHandler(
+      function(res){
+
+        if(res.success){
+
+          localStorage.setItem(
+            "token",
+            res.token
+          );
+
+          location.reload();
+
+        }else{
+
+          document
+            .getElementById("msg")
+            .innerHTML =
+            res.message;
+        }
+      }
+    )
+    .login(
+      username,
+      password
+    );
+}
