91916f4224b67afd59145ab91fb6f655b0236218
digitallycoded <ccbadvancedocs@gmail.com>
2026-06-06 14:08:38 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
+const GAS_URL =
+  "https://script.google.com/macros/s/AKfycbw4_EdbH04pf7m7WnliwdVUGOOifZY8eE2QKdgVD686DSNVL_-CUSy434usgJhXnm-9/exec";
 
+async function login(){
+
+  const response = await fetch(GAS_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "login",
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    })
+  });
+
+  const result = await response.json();
+
+  console.log(result);
+}
